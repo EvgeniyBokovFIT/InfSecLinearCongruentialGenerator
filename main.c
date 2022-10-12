df71d5ebbb5842d72df4b806d1a083c14458265c
EvgeniyBokovFIT <zheka2001941@gmail.com>
2022-10-12 16:40:09 +0700

    read line from stdin

--- main.c
+++ main.c
@@ -3,14 +3,41 @@
 #include <string.h>
 #include "LinearCongruentialGenerator.h"
 
-int main() {
-    char* plaintext = "message";
-    char* ciphertext = malloc(strlen(plaintext) * sizeof(char));
-    char* decrypted_text = malloc(strlen(ciphertext) * sizeof(char));
-    encrypt(plaintext, ciphertext);
-    printf("%s\n", ciphertext);
-    decrypt(ciphertext, decrypted_text);
-    printf("%s\n", decrypted_text);
+int main(int argc, char** argv) {
+
+
+    int input_size;
+
+    char *end;
+
+    printf("%s\n", "Enter the input size");
+    char size_buf[10];
+    while (fgets(size_buf, sizeof(size_buf), stdin)) {
+        input_size = strtol(size_buf, &end, 10);
+        if (end == size_buf || *end != '\n') {
+            printf("Please enter an integer: ");
+        } else break;
+    }
+    char* input_text = malloc(input_size);
+
+    printf("%s\n", "Enter the input text");
+    gets(input_text);
+
+    char mode_buf[1];
+    char mode;
+
+    printf("%s\n", "Enter the mode c/d");
+
+    scanf(" %c",&mode);
+
+    char* result_text = malloc(strlen(input_text) * sizeof(char));
+    if(mode == 'c') {
+        encrypt(input_text, result_text);
+    } else {
+        decrypt(input_text, result_text);
+    }
+
+    printf("%s\n", result_text);
 
     return 0;
 }
